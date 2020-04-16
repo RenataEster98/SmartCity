0957048781d1ff48c2efbd903c4febd68ba0ff40
Renata <ester.b@live.com>
2020-04-16 01:20:48 -0300

    Criaçao de Controller TipoProduto

--- FiapSmartCity/Models/TipoProduto.cs
+++ FiapSmartCity/Models/TipoProduto.cs
@@ -7,5 +7,8 @@ namespace FiapSmartCity.Models
 {
     public class TipoProduto
     {
+        public int IdTipo { get; set; }
+        public String DescricaoTipo { get; set; }
+        public bool Comercializado { get; set; }
     }
 }
